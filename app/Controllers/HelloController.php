34a71b522e1b8b4a85c3677b243f50fe34ab1678
yukimafuyuuenoyama@gmail.com <yukimafuyuuenoyama@gmail.com>
2023-05-28 22:32:35 +0700

    pertama commit

---
+++ app/Controllers/HelloController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class HelloController extends BaseController
+{
+    public function index()
+    {
+        echo "Hello World";
+
+
+    }
+}
